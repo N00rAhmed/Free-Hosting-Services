b2e7125ba99b4ec3c84b4d2989cd49bb86e8e768
N00rAhmed <tronn232003@gmail.com>
2023-11-09 22:18:43 +0000

    installed tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
     <div className='container'>
       <Navbar/>
       <h1>Free Hosting Services for devs </h1>
+{/* for tailwind make sure to use single quotoations */}
+      {/* <h1 className='text-red-800 bg-blue-900'>
+      Hello world!
+    </h1> */}
+
       <div className='intro-container'>
         <p className='intro'>Welcome to Free Hosting Services for Devs!
           Here you will find hosting services without any cost. 

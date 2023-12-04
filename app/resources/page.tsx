84ab37b6a18d4061127a34c428a72049322169d7
N00rAhmed <tronn232003@gmail.com>
2023-12-04 14:40:48 +0000

    moved data to json file

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,7 +1,9 @@
 import Navbar from '@/app/navbar/page';
 import '../resources/resources.css';
+import data from '../data.json';
 
-export default function page() {
+export default async function page() {
+
   return (
     <div>
       <Navbar/>
@@ -15,65 +17,15 @@ export default function page() {
       <div className='resource-info'>
         
         <ul>
-            {/* <li><b>Services:</b> FreeClientSide</li> */}
-            <h3>make sure to check these sites work and are free </h3>
-              <li>Netlify</li>
-              <li>Vercel</li>
-              <li>Render.com</li>
-              <li>GitHub Pages</li>
-              <li>Supabase</li>
-              <li>CockroachDB</li>
-              <li>MongoDB</li>
-              <li>freemysqlhosting.net</li>
-              <li>freeasphosting.net</li>
-              <li>freesqldatabase.com</li>
-              <li>freedb.tech</li>
-              <li>surge.sh</li>
-              <li>cyclic.sh</li>
-              <li>https://fleek.co/hosting/</li>
-              <li>https://pages.cloudflare.com</li>
-              <li>w3schools.com/spaces/index.php</li>
-              <li>firebase</li>
-              <li>digitalocean.com</li>
-              <li>alwaysdata.com</li>
-              <li>https://bohr.io</li>
-              <li>https://bubble.io</li>
-              <li>https://dappling.network</li>
-              <li>awardspace.net</li>
-              <li>https://www.drv.tw</li>
-              <li>duckdocs.com</li>
-              <li>https://freehostingnoads.net</li>
-              <li>https://kinsta.com</li>
-              <li>https://neocities.org</li>
-              <li>https://about.readthedocs.com</li>
-              <li>https://app.qoddi.com</li>
-              <li>https://mdbgo.com</li>
-              <li>https://patr.cloud</li>
-              <li>https://www.serv00.com</li>
-              <li>https://x10hosting.com</li>
-              <li>https://byet.host</li>
-              <li>https://www.000webhost.com</li>
-              <li>infinityfree.com</li>
-              <li>https://nhost.io</li>
-              <li>https://deno.com/deploy</li>
-              <li>https://www.microtica.com</li>
-              <li>https://www.back4app.com</li>
-              <li>https://pocketbase.io</li>
-              <li>https://tiiny.host</li>
-              <li>https://www.pythonanywhere.com</li>
-              <li></li>
-              <li></li>
-              <h3>links on these websites to check</h3>
-              <li>https://levelup.gitconnected.com/free-hosting-providers-in-2023-a984f9fb955e</li>
-              <li>https://www.patrickxchong.com/deploy-websites-and-apis-for-free/</li>
-              <li>https://faun.pub/18-sites-to-host-your-backend-code-for-free-d3463a814b93</li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            {/* <h3>make sure to check these sites work and are free </h3> */}
+            {data.map((item, index) => (
+            <li key={index}>
+              <a href={item.link} target="_blank"><strong>{item.title}</strong></a>
+              <p>{item.content}</p>
+            </li>
+          ))}
 
-            </ul>
+        </ul>
 
       </div>
 

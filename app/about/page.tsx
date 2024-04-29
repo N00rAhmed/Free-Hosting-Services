075ceb515e097c9f695b5a708b5d45360230c02a
N00rAhmed <tronn232003@gmail.com>
2024-04-29 22:47:10 +0100

    small change

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,7 @@ export default function about() {
 
       <div className='about-container'>
         <strong className='about-info'>The development of this website started in October 2023. The main purpose of this project is to provide a platform for 
-        sharing knowledge and resources related to hosting web development projects, servers, API&apos;s, SQL/NOSQL databases at no cost. 
+        sharing resources related to hosting web development projects, servers, API&apos;s, SQL/NOSQL databases at no cost. 
         </strong>
       </div>
       

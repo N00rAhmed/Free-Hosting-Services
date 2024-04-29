9d9b7643c69d42dc898c38adb4f876fa93c0d315
N00rAhmed <tronn232003@gmail.com>
2024-04-29 22:45:49 +0100

    small change

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -60,11 +60,11 @@ export default function Page() {
               <p className="font-bold">Languages/Frameworks: {item.technologies}</p>
               <br />
 
-              <button
+              {/* <button
                 type="button" 
                 className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                 Instructions
-              </button>
+              </button> */}
 
               <ArticleIcon />
 
